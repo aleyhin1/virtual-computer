86227cfb7ee80ae1414806aa721314d16dae8d72
Batuhan Akyürek <batuhan@loopgames.net>
2026-03-17 16:32:13 +0300

    mic1 control store created

--- computer.c
+++ computer.c
@@ -5,17 +5,6 @@
 
 int main()
 {
-    Ram1x2* r = ram1x2_create();
-
-    bool r0 = ram1x2_read_write(r, 1, 0, 1, 0, 0);
-    bool r1 = ram1x2_read_write(r, 0, 1, 1, 1, 1);
     
-    r0 = ram1x2_read_write(r, 0, 0, 1, 1, 0);
-
-    printf("%d\n", r0);
-    printf("%d\n", r1);
-
-    ram1x2_destroy(r);
-
     return 0;
 }
---
+++ microarchitecture.c
@@ -0,0 +1,2 @@
+#include "microarchitecture.h"
+
